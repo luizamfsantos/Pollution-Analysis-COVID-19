2323cee9793fd26e30cfc5510953e1cc6ac0cd30
Luiza M. F. dos Santos <59169172+luizamfsantos@users.noreply.github.com>
2020-04-26 09:54:45 -0500

    change relative path

--- data/air_quality_San_Diego.R
+++ data/air_quality_San_Diego.R
@@ -3,7 +3,7 @@ my_packages <- c("readr","ggplot2")
 lapply(my_packages,library,character.only = TRUE)
 
 # import data
-san_diego_data_aqi_2019 <- read_csv('air_quality/san_diego_2019_aqi',
+san_diego_data_aqi_2019 <- read_csv('data/air_quality/san_diego_2019_aqi',
                                     col_types = 
                                       date_local = col_date(format='%m/%d/%Y'),
                                       aqi = col_double())
